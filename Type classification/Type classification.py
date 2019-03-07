8aa21dc1fcf2cbd52cb6f2e99b3752f6a10c1ac3
PARKYOONJUNG <201602001@o.cnu.ac.kr>
2019-03-07 17:57:18 +0900

    타입별 그래프/파일
    타입별 그래프/파일

---
+++ Type classification/Type classification.py
@@ -0,0 +1,304 @@
+
+# coding: utf-8
+
+# In[38]:
+
+
+import pymysql
+import pandas as pd
+from pandas import DataFrame, Series
+
+from sqlalchemy import create_engine
+
+import matplotlib
+
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm 
+
+font_location = 'C:\\Users\\User\\Jupyter\\malgun.ttf'
+
+font_name = fm.FontProperties(fname = font_location).get_name()
+matplotlib.rc('font', family = font_name)
+matplotlib.rcParams.update({'font.size': 22})
+
+
+# In[39]:
+
+
+class MySQLConnectorWithPandas :
+    
+    global conn
+    
+    def make_connect (self, dbname) :
+        print("start db connect!")
+        engine = create_engine( """mysql+pymysql://root:1234@147.43.122.131/"""+dbname+"""?charset=utf8""", encoding = "utf8")
+        self.conn = engine.connect()
+
+    def read_by_table (self, table_name) :
+        data = pd.read_sql_table(table_name, self.conn)
+
+        return data
+
+db = MySQLConnectorWithPandas()
+
+
+# In[40]:
+
+
+matplotlib.rcParams.update({'font.size': 22})
+
+
+# In[41]:
+
+
+def input_value () : 
+    mode = input("naver or daum ?")
+    database = input("Database ? ")
+    
+    start_year = str(input("Start Year ? "))
+    start_month = str(input("Start Month ? "))
+    start_day = str(input("Start Day ? "))
+
+    end_year = str(input("End Year ? "))
+    end_month = str(input("End Month ? "))
+    end_day = str(input("End Day ? "))
+    
+    mode_database = mode+ '_' + database
+    
+    conn = db.make_connect(mode_database)
+    news = db.read_by_table(mode + "_articles")
+    news_df = DataFrame(news)
+    
+    news_df.sort_values(by = ['article_date'], axis = 0, inplace = True)
+    news_df = news_df[news_df.article_date != '-']
+    news_df = news_df.dropna()
+    
+    if(mode == 'naver') : 
+        news_df['article_date'] = news_df['article_date'].str.replace('최종수정 ', '')
+        news_df['article_date'] = news_df['article_date'].str.replace('.', '-')
+        
+        start_date = start_year + '-' + start_month + '-' + start_day
+        end_date = end_year + '-' + end_month + '-' + end_day
+    
+    elif(mode == 'daum') : 
+        news_df['article_date'] = news_df['article_date'].str.replace('수정 ', '')
+        news_df['article_date'] = news_df['article_date'].str.replace('입력 ', '')
+        
+        start_date = start_year + '.' + start_month + '.' + start_day
+        end_date = end_year + '.' + end_month + '.' + end_day
+    
+    news_df = news_df[news_df.article_date > start_date]
+    news_df = news_df[news_df.article_date < end_date]
+    
+    return news_df
+
+
+# In[ ]:
+
+
+def make_file() :
+    
+
+
+# In[ ]:
+
+
+def make_Graph(news_df) :
+    # 첫번째와 마지막 값 추출해야함
+    year = ["2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010"]
+
+    it_science = []
+    economy = []
+    social = []
+    living_culture = []
+    world = []
+    politics = []
+
+    news_df['article_date'] = news_df['article_date'].apply(lambda x : str(x[0:4]))
+
+    for i in year :
+        temp_df = news_df[news_df.article_date == i]
+        temp_df = temp_df.groupby('article_type').size().to_frame('T_count')
+
+        if(temp_df[temp_df.index == "IT/과학"].count().item() > 0) :
+            it_science.append(temp_df.ix['IT/과학'].values[0])
+        else :
+            it_science.append(0)
+
+        if(temp_df[temp_df.index == "경제"].count().item() > 0) :
+            economy.append(temp_df.ix['경제'].values[0])
+        else :
+            economy.append(0)
+
+        if(temp_df[temp_df.index == "사회"].count().item() > 0) :
+            social.append(temp_df.ix['사회'].values[0])
+        else :
+            social.append(0)
+
+        if(temp_df[temp_df.index == "생활/문화"].count().item() > 0) :
+            living_culture.append(temp_df.ix['생활/문화'].values[0])
+        else :
+            living_culture.append(0)
+
+        if(temp_df[temp_df.index == "세계"].count().item() > 0) :
+            world.append(temp_df.ix['세계'].values[0])
+        else :
+            world.append(0)
+
+        if(temp_df[temp_df.index == "정치"].count().item() > 0) :
+            politics.append(temp_df.ix['정치'].values[0])
+        else :
+            politics.append(0)
+
+    ax = plt.figure(figsize = (30, 20))
+    ax = plt.subplot(111)
+
+    plt.plot(year, it_science, label = "IT/과학")
+    plt.plot(year, economy, label = "경제")
+    plt.plot(year, social, label = "사회")
+    plt.plot(year, living_culture, label = "생활/문화")
+    plt.plot(year, world, label = "세계")
+    plt.plot(year, politics, label = "정치")
+
+    plt.plot(year, it_science, label = "IT/과학")
+    for i,j in zip(year, it_science):
+        ax.annotate(str(j),xy=(i,j))
+    plt.plot(year, economy, label = "경제")
+    for i,j in zip(year, economy):
+        ax.annotate(str(j),xy=(i,j))
+    plt.plot(year, social, label = "사회")
+    for i,j in zip(year, social):
+        ax.annotate(str(j),xy=(i,j))
+    plt.plot(year, living_culture, label = "생활/문화")
+    for i,j in zip(year, living_culture):
+        ax.annotate(str(j),xy=(i,j))
+    plt.plot(year, world, label = "세계")
+    for i,j in zip(year, world):
+        ax.annotate(str(j),xy=(i,j))
+    plt.plot(year, politics, label = "정치")
+    for i,j in zip(year, politics):
+        ax.annotate(str(j),xy=(i,j))
+
+    plt.xlabel('Year')
+    plt.ylabel('Count')
+
+    plt.title('Number for News Type')
+
+    plt.legend()
+
+    plt.show()
+
+
+# In[42]:
+
+
+news_df = input_value()
+make_Graph(news_df)
+# 그림 파일로 저장하는 거 추가
+
+select_type = []
+saved_file =  input("cvs 파일로 저장하시겠습니까 ? y/n ")
+if (saved_file == 'y') :
+    file_num = input("몇 개를 파일로 저장하시겠습니까 ?")
+    while file_num > 0 :
+        num = input("파일로 저장할 분야? 1.IT/과학 2.경제 3.사회 4.생활/문화 5.세계 6.정치 ")
+        select_type.append(num)
+        file_num -= 1
+    make_file(news_df,select_type)    
+    print("파일 저장 후 Finish!")
+else :
+    print("파일 저장 안하고 Finish!")
+    exit()
+
+
+# In[43]:
+
+
+year = ["2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010"]
+
+it_science = []
+economy = []
+social = []
+living_culture = []
+world = []
+politics = []
+
+news_df['article_date'] = news_df['article_date'].apply(lambda x : str(x[0:4]))
+
+for i in year :
+    temp_df = news_df[news_df.article_date == i]
+    temp_df = temp_df.groupby('article_type').size().to_frame('T_count')
+    
+    if(temp_df[temp_df.index == "IT/과학"].count().item() > 0) :
+        it_science.append(temp_df.ix['IT/과학'].values[0])
+    else :
+        it_science.append(0)
+        
+    if(temp_df[temp_df.index == "경제"].count().item() > 0) :
+        economy.append(temp_df.ix['경제'].values[0])
+    else :
+        economy.append(0)
+    
+    if(temp_df[temp_df.index == "사회"].count().item() > 0) :
+        social.append(temp_df.ix['사회'].values[0])
+    else :
+        social.append(0)
+     
+    if(temp_df[temp_df.index == "생활/문화"].count().item() > 0) :
+        living_culture.append(temp_df.ix['생활/문화'].values[0])
+    else :
+        living_culture.append(0)
+        
+    if(temp_df[temp_df.index == "세계"].count().item() > 0) :
+        world.append(temp_df.ix['세계'].values[0])
+    else :
+        world.append(0)
+        
+    if(temp_df[temp_df.index == "정치"].count().item() > 0) :
+        politics.append(temp_df.ix['정치'].values[0])
+    else :
+        politics.append(0)
+        
+
+
+# In[45]:
+
+
+ax = plt.figure(figsize = (30, 20))
+ax = plt.subplot(111)
+
+plt.plot(year, it_science, label = "IT/과학")
+plt.plot(year, economy, label = "경제")
+plt.plot(year, social, label = "사회")
+plt.plot(year, living_culture, label = "생활/문화")
+plt.plot(year, world, label = "세계")
+plt.plot(year, politics, label = "정치")
+
+plt.plot(year, it_science, label = "IT/과학")
+for i,j in zip(year, it_science):
+    ax.annotate(str(j),xy=(i,j))
+plt.plot(year, economy, label = "경제")
+for i,j in zip(year, economy):
+    ax.annotate(str(j),xy=(i,j))
+plt.plot(year, social, label = "사회")
+for i,j in zip(year, social):
+    ax.annotate(str(j),xy=(i,j))
+plt.plot(year, living_culture, label = "생활/문화")
+for i,j in zip(year, living_culture):
+    ax.annotate(str(j),xy=(i,j))
+plt.plot(year, world, label = "세계")
+for i,j in zip(year, world):
+    ax.annotate(str(j),xy=(i,j))
+plt.plot(year, politics, label = "정치")
+for i,j in zip(year, politics):
+    ax.annotate(str(j),xy=(i,j))
+            
+plt.xlabel('Year')
+plt.ylabel('Count')
+
+plt.title('Number for News Type')
+
+plt.legend()
+
+plt.show()
+
